03f48ed0ddbef279ac443364530814ab5b82df4f
Max Kuhn <mxkuhn@gmail.com>
2022-05-09 20:18:10 -0400

    initial files

---
+++ R/on_goal.R
@@ -0,0 +1,15 @@
+#' Shots on goal data
+#'
+#' These data are for Pittsburgh Penguins games (seasons 2015-2016, 2016-2017,
+#' and 2017-2018). Shots were determined to be on goal (i.e., made it to the
+#' goaltender) or not (i.e., missed or blocked).
+#'
+#' @name on_goal
+#' @aliases on_goal
+#' @docType data
+#' @return \item{on_goal}{a tibble}
+#'
+#' @keywords datasets
+#' @examples
+#' str(on_goal)
+NULL
